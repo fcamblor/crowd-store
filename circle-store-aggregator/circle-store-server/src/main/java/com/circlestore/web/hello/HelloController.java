fed0caa647d761548a08934fa0b2110bf1b8f3ce
Frédéric Camblor <fcamblor@gmail.com>
2012-09-20 02:28:10 +0200

    added controller for demonstration

---
+++ circle-store-aggregator/circle-store-server/src/main/java/com/circlestore/web/hello/HelloController.java
@@ -0,0 +1,82 @@
+package com.circlestore.web.hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author fcamblor
+ */
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    private static Logger LOG = LoggerFactory.getLogger(HelloController.class);
+
+    public static class Result {
+        Integer value;
+        public Integer getValue() {
+            return value;
+        }
+        public void setValue(Integer value) {
+            this.value = value;
+        }
+    }
+
+    public static class Query {
+        @NotNull
+        Integer leftOperand;
+        @NotNull
+        Integer rightOperand;
+        public Integer getLeftOperand() {
+            return leftOperand;
+        }
+        public void setLeftOperand(Integer leftOperand) {
+            this.leftOperand = leftOperand;
+        }
+        public Integer getRightOperand() {
+            return rightOperand;
+        }
+        public void setRightOperand(Integer rightOperand) {
+            this.rightOperand = rightOperand;
+        }
+    }
+
+    public static enum Operator {
+        plus {
+            @Override
+            public Integer apply(Integer leftOperand, Integer rightOperand) {
+                return leftOperand + rightOperand;
+            }
+        }, minus {
+            @Override
+            public Integer apply(Integer leftOperand, Integer rightOperand) {
+                return leftOperand - rightOperand;
+            }
+        }, multiply {
+            @Override
+            public Integer apply(Integer leftOperand, Integer rightOperand) {
+                return leftOperand * rightOperand;
+            }
+        }, divide {
+            @Override
+            public Integer apply(Integer leftOperand, Integer rightOperand) {
+                return Math.round(leftOperand / rightOperand);
+            }
+        };
+        public abstract Integer apply(Integer leftOperand, Integer rightOperand);
+    }
+
+    @RequestMapping(value="/calculate",method=RequestMethod.POST)
+    public @ResponseBody
+    Result calculate(@RequestBody @Validated Query query){
+        LOG.info("In calculate() ...");
+        Result r = new Result();
+        r.value = Operator.plus.apply(query.leftOperand, query.rightOperand);
+        return r;
+    }
+}
