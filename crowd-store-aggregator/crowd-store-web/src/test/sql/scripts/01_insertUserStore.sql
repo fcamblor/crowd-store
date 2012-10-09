61c3e6d1c250bd01a7eea6a7be6239f327675dcf
Frédéric Camblor <fcamblor@gmail.com>
2012-10-09 09:29:42 +0200

    fix intellij reformat code bad practices in sql script (renaming tables with uppercases, adding spaces around table names with `` ...)

--- crowd-store-aggregator/crowd-store-web/src/test/sql/scripts/01_insertUserStore.sql
+++ crowd-store-aggregator/crowd-store-web/src/test/sql/scripts/01_insertUserStore.sql
@@ -1,33 +1,33 @@
-INSERT INTO USER(email, password, first_name, last_name, logically_deleted,private_token, public_token)
+INSERT INTO user(email,password,first_name,last_name,logically_deleted,private_token,public_token)
 VALUES
 (
-'t@t.fr',
-'toto',
-'Damien',
-'RICCIO',
-FALSE,
-'0',
-'0'
+  't@t.fr',
+  'toto',
+  'Damien',
+  'RICCIO',
+  FALSE,
+  '0',
+  '0'
 );
 
 INSERT INTO store
 (
-NAME,
-logically_deleted
+  name,
+  logically_deleted
 )
 VALUES
 (
-'4sh store',
-FALSE
+  '4sh store',
+  FALSE
 );
 
 INSERT INTO user_store
 (
-user_id,
-store_id,
-role
+  user_id,
+  store_id,
+  role
 )
 SELECT u.id, s.id, 'CUSTOMER'
-FROM USER u, store s
-WHERE u.email = 't@t.fr' AND s.name = '4sh store'
+FROM user u, store s
+WHERE u.email='t@t.fr' and s.name='4sh store'
 ;
