02fe23dcefa276eec28e76815969bb2127bd2f41
Frédéric Camblor <fcamblor@gmail.com>
2012-10-14 23:59:39 +0200

    Provided AuthenticatedUser dedicated subclass to track UserStoreRole adding to AuthenticatedUser during MyBatis processing of findAuthenticatedUserByCredentials query, because mybatis doesn't seem to allow to inject a collection "in the end of processing" (see https://groups.google.com/forum/?fromgroups=#!topic/mybatis-user/myPouCEgF5c) I hope solution will be found because current code is really crappy !

--- crowd-store-aggregator/crowd-store-web/src/main/java/com/crowdstore/persistence/user/UserDaoImpl.java
+++ crowd-store-aggregator/crowd-store-web/src/main/java/com/crowdstore/persistence/user/UserDaoImpl.java
@@ -6,13 +6,54 @@ import com.crowdstore.persistence.common.DaoSupport;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author damienriccio
  */
 @Repository
 public class UserDaoImpl extends SqlSessionDaoSupport implements UserDao {
+
+    /**
+     * Temporary class used to provide a workaround for a problem encountered
+     * and described here : https://groups.google.com/forum/?fromgroups=#!topic/mybatis-user/myPouCEgF5c
+     * Not really proud of this, but it doesn't seem we can do better...
+     */
+    public static class TmpAuthenticatedUser extends AuthenticatedUser {
+        private List<UserStoreRole> storeRoles;
+
+        protected TmpAuthenticatedUser(){
+            this.storeRoles = new ArrayList<UserStoreRole>(){
+                @Override
+                public boolean add(UserStoreRole userStoreRole) {
+                    boolean res = super.add(userStoreRole);
+                    getStoresAuthorizations().put(userStoreRole.getStoreName(), userStoreRole.getRole().getAuthorizations());
+                    return res;
+                }
+
+                @Override
+                public void add(int index, UserStoreRole userStoreRole) {
+                    super.add(index, userStoreRole);
+                    getStoresAuthorizations().put(userStoreRole.getStoreName(), userStoreRole.getRole().getAuthorizations());
+                }
+            };
+        }
+
+        @Override
+        public AuthenticatedUser setStoreRoles(List<UserStoreRole> storeRoles) {
+            this.storeRoles.addAll(storeRoles);
+            return super.setStoreRoles(storeRoles);
+        }
+
+        public List<UserStoreRole> getStoreRoles(){
+            return this.storeRoles;
+        }
+    }
+
     @Override
     public AuthenticatedUser findAuthenticatedUserByCredentials(Credentials credentials) {
-        return DaoSupport.selectOne(this, "findAuthenticatedUserByCredentials", credentials);
+        TmpAuthenticatedUser tmpAuthenticatedUser = DaoSupport.selectOne(this, "findAuthenticatedUserByCredentials", credentials);
+        return tmpAuthenticatedUser;
     }
 }
