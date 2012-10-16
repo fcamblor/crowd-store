b380c6b3751cbc303b7cec40bf9647d27ebd6e5d
Frédéric Camblor <fcamblor@gmail.com>
2012-10-17 01:53:28 +0200

    added attachUserToStore in StoreDao

--- crowd-store-aggregator/crowd-store-web/src/main/java/com/crowdstore/persistence/store/StoreDaoImpl.java
+++ crowd-store-aggregator/crowd-store-web/src/main/java/com/crowdstore/persistence/store/StoreDaoImpl.java
@@ -1,12 +1,15 @@
 package com.crowdstore.persistence.store;
 
+import com.crowdstore.models.role.StoreRole;
 import com.crowdstore.models.store.FlatStore;
 import com.crowdstore.models.users.UserIdentity;
 import com.crowdstore.persistence.common.DaoSupport;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author damienriccio
@@ -26,5 +29,14 @@ public class StoreDaoImpl extends SqlSessionDaoSupport implements StoreDao {
     @Override
     public void hardDeleteStoresByNames(String... storeNames) {
         DaoSupport.delete(this, "deleteStoresByNames", storeNames);
+    }
+
+    @Override
+    public void attachUserToStore(Long userId, String storeName, StoreRole storeRole) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("userId", userId);
+        params.put("storeName", storeName);
+        params.put("role", storeRole.name());
+        DaoSupport.insert(this, "attachUserToStore", params);
     }
 }
