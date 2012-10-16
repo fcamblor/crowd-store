00382641be20b1c2bda30bc509a2c54c5b11e2f6
Frédéric Camblor <fcamblor@gmail.com>
2012-10-17 01:52:26 +0200

    Added CRUD user services

--- crowd-store-aggregator/crowd-store-web/src/main/java/com/crowdstore/service/user/UserService.java
+++ crowd-store-aggregator/crowd-store-web/src/main/java/com/crowdstore/service/user/UserService.java
@@ -2,10 +2,13 @@ package com.crowdstore.service.user;
 
 import com.crowdstore.models.auth.Credentials;
 import com.crowdstore.models.users.AuthenticatedUser;
+import com.crowdstore.models.users.FlatUser;
 
 /**
  * @author fcamblor
  */
 public interface UserService {
     AuthenticatedUser authenticate(Credentials credentials) throws AuthenticationError;
+    void createUser(FlatUser user);
+    void hardDeleteUsersByIds(Long... userIds);
 }
--- crowd-store-aggregator/crowd-store-web/src/main/java/com/crowdstore/service/user/UserServiceImpl.java
+++ crowd-store-aggregator/crowd-store-web/src/main/java/com/crowdstore/service/user/UserServiceImpl.java
@@ -2,10 +2,14 @@ package com.crowdstore.service.user;
 
 import com.crowdstore.models.auth.Credentials;
 import com.crowdstore.models.users.AuthenticatedUser;
+import com.crowdstore.models.users.FlatUser;
 import com.crowdstore.persistence.user.UserDao;
+import com.crowdstore.service.security.PermissionsService;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+
+import static com.crowdstore.common.security.Permissions.ensure;
 
 /**
  * @author fcamblor
@@ -16,6 +20,9 @@ public class UserServiceImpl implements UserService {
     @Inject
     UserDao userDao;
 
+    @Inject
+    PermissionsService permissionsService;
+
     @Override
     public AuthenticatedUser authenticate(Credentials credentials) throws AuthenticationError {
         AuthenticatedUser authenticatedUser = userDao.findAuthenticatedUserByCredentials(credentials);
@@ -23,5 +30,27 @@ public class UserServiceImpl implements UserService {
             throw new AuthenticationError("Bad credentials !");
         }
         return authenticatedUser;
+    }
+
+    @Override
+    public void createUser(FlatUser user) {
+        ensure(permissionsService.canCreateNewUser(), "Cannot create new user");
+
+        AuthenticatedUser existingUser = userDao.findAuthenticatedUserByPrivateToken(user.getPrivateToken());
+        if(existingUser != null){
+            throw new IllegalArgumentException("Given private token is not unique");
+        }
+
+        userDao.createUser(user);
+    }
+
+    @Override
+    public void hardDeleteUsersByIds(Long... userIds) {
+        ensure(permissionsService.canDeleteUsers(), "Cannot delete users");
+
+        // Detaching entities linked to users
+        userDao.detachStoresToUsers(userIds);
+
+        userDao.hardDeleteUsersByIds(userIds);
     }
 }
