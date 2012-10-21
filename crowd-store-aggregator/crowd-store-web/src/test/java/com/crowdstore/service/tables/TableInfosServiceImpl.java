f911c9d57289cec65fbd7a1775323bb10fb9a851
Frédéric Camblor <fcamblor@gmail.com>
2012-10-22 01:36:26 +0200

    TableInfoService.getTableInfos() should be transactional

--- crowd-store-aggregator/crowd-store-web/src/test/java/com/crowdstore/service/tables/TableInfosServiceImpl.java
+++ crowd-store-aggregator/crowd-store-web/src/test/java/com/crowdstore/service/tables/TableInfosServiceImpl.java
@@ -1,7 +1,10 @@
 package com.crowdstore.service.tables;
 
 import com.crowdstore.persistence.tables.TableInfosDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
 import java.util.List;
@@ -12,10 +15,13 @@ import java.util.List;
 @Service
 public class TableInfosServiceImpl implements TableInfosService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TableInfosServiceImpl.class);
+
     @Inject
     TableInfosDao tableInfosDao;
 
     @Override
+    @Transactional
     public List<TableInfosDao.TableInfos> getTableInfos() {
         return tableInfosDao.getTableInfos();
     }
