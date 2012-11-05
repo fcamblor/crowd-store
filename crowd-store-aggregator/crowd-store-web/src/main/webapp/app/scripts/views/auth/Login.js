e2d7591c0a768799db38a574de940dc01c82d2fc
Frédéric Camblor <fcamblor@gmail.com>
2012-11-05 02:16:40 +0100

    added example triggered on login submit button click

--- crowd-store-aggregator/crowd-store-web/src/main/webapp/app/scripts/views/auth/Login.js
+++ crowd-store-aggregator/crowd-store-web/src/main/webapp/app/scripts/views/auth/Login.js
@@ -1,7 +1,16 @@
 define(["hbs!templates/auth/login"], function(tmplContent){
     return Backbone.View.extend({
+        id: "loginForm",
+        events: {
+            "click #authenticateBtn": "authenticate"
+        },
+
         render: function(){
             return tmplContent({});
+        },
+
+        authenticate: function(){
+            alert("Submit action detected !");
         }
     });
 });
