dbad7ce5b6422fb7094a4cf380c89708a4afd001
Frédéric Camblor <fcamblor@gmail.com>
2012-11-05 02:52:13 +0100

    Illustrating use of backbone deep models and automatic model binding

--- crowd-store-aggregator/crowd-store-web/src/main/webapp/app/scripts/views/auth/Login.js
+++ crowd-store-aggregator/crowd-store-web/src/main/webapp/app/scripts/views/auth/Login.js
@@ -1,16 +1,28 @@
-define(["hbs!templates/auth/login"], function(tmplContent){
+define(["hbs!templates/auth/login", "backbone"], function(tmplContent, Backbone){
     return Backbone.View.extend({
         events: {
             "click #authenticateBtn": "authenticate"
         },
 
+        initialize: function(){
+            this.model = new Backbone.DeepModel();
+            this.model.set({ credentials: { login: "foo@bar.com" } });
+
+            this._modelBinder = new Backbone.ModelBinder();
+        },
+
+        close: function(){
+            this._modelBinder.unbind();
+        },
+
         render: function(){
             this.$el.html(tmplContent({}));
+            this._modelBinder.bind(this.model, this.el);
             return this;
         },
 
         authenticate: function(){
-            alert("Submit action detected !");
+            alert("Submit action detected with following data : "+JSON.stringify(this.model.toJSON()));
         }
     });
 });
