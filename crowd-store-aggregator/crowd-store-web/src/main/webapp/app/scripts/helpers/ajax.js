acdf3bf6041b6cb6b9573304ed94a95080a58f29
Frédéric Camblor <fcamblor@gmail.com>
2012-11-07 01:47:25 +0100

    plugged $.displaySpringErrors on 412 http response codes to handle validation errors

--- crowd-store-aggregator/crowd-store-web/src/main/webapp/app/scripts/helpers/ajax.js
+++ crowd-store-aggregator/crowd-store-web/src/main/webapp/app/scripts/helpers/ajax.js
@@ -1,10 +1,30 @@
-define(["jquery", "underscore", "underscore.string"], function($, _, _s){
+define(["jquery", "underscore", "underscore.string", "helpers/js-utils"], function($, _, _s, JsUtils){
     return {
         initJQueryAjax: function(app){
+            // Handling cross domain urls, particularly between yeoman server
+            // and jee server
             var crossDomainRootUrl = app.crossDomainRootUrl();
-            $.ajaxPrefilter(function(options, originalOptions, jqXHR){
-                if(crossDomainRootUrl && !_s.startsWith(options.url, "http")){
-                    options.url = crossDomainRootUrl + options.url;
+            if(crossDomainRootUrl){
+                $.ajaxPrefilter(function(options, originalOptions, jqXHR){
+                    if(!_s.startsWith(options.url, "http")){
+                        options.url = crossDomainRootUrl + options.url;
+                    }
+                });
+            }
+
+            // This prevent jquery to add square brackets [] at the end of param name when they have multiple values,
+            // which is breaking spring binding.
+            $.ajaxSettings.traditional = true;
+
+            /**
+             * Overriding default behaviour for ajax errors : looking at jquery XMLHttpRequest's status
+             * and, if it is the special 412 (Precondition failed) status _and_ response is a JSON representation,
+             * then call $.displaySpringErrors()
+             */
+            $(document).ajaxError(function(event, jqXHR, settings, exception){
+                if(jqXHR.status === 412 && JsUtils.isJSON(jqXHR.responseText)){
+                    var springBindingResults = $.parseJSON(jqXHR.responseText);
+                    $.displaySpringErrors(springBindingResults);
                 }
             });
         }
