d5110dbf7bcd35c5b508be9d2f2cffa56e447eda
Frédéric Camblor <fcamblor@gmail.com>
2012-11-08 00:18:09 +0100

    updated dataset inserts, adding ignore param on them

--- crowd-store-aggregator/crowd-store-web/src/test/sql/scripts/01_dataSet.sql
+++ crowd-store-aggregator/crowd-store-web/src/test/sql/scripts/01_dataSet.sql
@@ -1,6 +1,5 @@
-        
-
-INSERT INTO user(email,hashed_password,first_name,last_name,logically_deleted,private_token)
+-- non hashed password: toto
+INSERT IGNORE INTO `user`(email,hashed_password,first_name,last_name,logically_deleted,private_token)
 VALUES
 (
   't@t.fr',
@@ -11,44 +10,32 @@ VALUES
   '0'
 );
 
-INSERT INTO store
-(
-  name,
-  logically_deleted
-)
-VALUES
-(
-  '4sh store',
-  FALSE
-);
+INSERT IGNORE INTO store (name, logically_deleted)
+VALUES ('4sh store', FALSE);
+INSERT IGNORE INTO store (name, logically_deleted)
+VALUES ('4sh soccer5', FALSE);
 
-INSERT INTO user_store
-(
-  user_id,
-  store_id,
-  role
-)
+INSERT IGNORE INTO `user_store` (user_id, store_id, role)
 SELECT u.id, s.id, 'CUSTOMER'
 FROM user u, store s
-WHERE u.email='t@t.fr' and s.name='4sh store'
-;
+WHERE u.email='t@t.fr' and s.name='4sh store';
 
 
-INSERT INTO product_category (name) VALUES ('Gazeux');
-INSERT INTO product_category (name) VALUES ('Naturel');
-INSERT INTO product_category (name) VALUES ('Chimique');
+INSERT IGNORE INTO product_category (name) VALUES ('Gazeux');
+INSERT IGNORE INTO product_category (name) VALUES ('Naturel');
+INSERT IGNORE INTO product_category (name) VALUES ('Chimique');
 
 
-INSERT INTO product (name, description) VALUES ('Liptonic', 'Thé glacé gazeux');
-INSERT INTO product (name, description) VALUES ('IceTea', 'Thé glacé');
+INSERT IGNORE INTO product (name, description) VALUES ('Liptonic', 'Thé glacé gazeux');
+INSERT IGNORE INTO product (name, description) VALUES ('IceTea', 'Thé glacé');
 
-INSERT INTO product_category_assoc (product_id, product_category_id) 
+INSERT IGNORE INTO product_category_assoc (product_id, product_category_id)
             select p.id, pc.id from product p, product_category pc where p.name='Liptonic' and pc.name='Gazeux';
-INSERT INTO product_category_assoc (product_id, product_category_id) 
+INSERT IGNORE INTO product_category_assoc (product_id, product_category_id)
             select p.id, pc.id from product p, product_category pc where p.name='IceTea' and pc.name='Naturel';
 
 
-INSERT INTO selling_product (product_id, store_id, price)
+INSERT IGNORE INTO selling_product (product_id, store_id, price)
             select p.id, s.id, 2 from product p, store s where p.name='Liptonic' and s.name='4sh store';
-INSERT INTO selling_product (product_id, store_id, price) 
+INSERT IGNORE INTO selling_product (product_id, store_id, price)
             select p.id, s.id, 3 from product p, store s where p.name='IceTea' and s.name='4sh store';
