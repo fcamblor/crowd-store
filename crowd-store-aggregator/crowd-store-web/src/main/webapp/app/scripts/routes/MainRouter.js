f05494e9f1a6e0990f4a89abcfaceb79b325cddb
Frédéric Camblor <fcamblor@gmail.com>
2012-11-15 01:53:52 +0100

    improved RouterMain, allowing to define path parameters in viewByUrl (sams syntax as the standard backbone one usable in routes field)

--- crowd-store-aggregator/crowd-store-web/src/main/webapp/app/scripts/routes/MainRouter.js
+++ crowd-store-aggregator/crowd-store-web/src/main/webapp/app/scripts/routes/MainRouter.js
@@ -15,27 +15,64 @@ define(["jquery", "backbone"], function($, Backbone){
             "auth/login": "auth/Login"
         },
 
+        // Will be calculated in initialize() depending on viewByUrl field
+        viewByUrlPattern: null,
+
         currentView: null,
 
+        initialize: function(){
+            Backbone.Router.prototype.initialize.call(this);
+
+            this.viewByUrlPattern = this.createViewByUrlPattern(this.viewByUrl);
+        },
+
         invokeView: function(url) {
-            var viewName = this.viewByUrl[url];
-            if(!viewName){
+            var viewInfos = this.findViewInfosByUrl(url);
+            if(!viewInfos){
                 throw "Missing view name declaration in viewByUrl for url : "+url;
             }
 
             // Requiring view and invoking render() on it
-            require(["views/"+viewName], function(ViewClass){
+            require(["views/"+viewInfos.viewName], function(ViewClass){
                 // Fixing memory leak, by cleaning previous view (and attached event on it)
                 if(this.currentView){
-                    this.currentView.remove();
+                    this.currentView.undelegateEvents();
+                    this.currentView.$el.empty();
                 }
 
                 // Instantiating view..
-                this.currentView = new ViewClass();
+                this.currentView = new ViewClass(viewInfos.parameters);
                 this.currentView.setElement(dynamicSection);
                 // ... then calling render() on it
                 this.currentView.render();
             });
+        },
+
+        findViewInfosByUrl: function(url) {
+            var urlPatterns = this.viewByUrlPattern;
+            var matchingViewUrlPattern = _.find(_.keys(urlPatterns), function(routeUrl){
+                return urlPatterns[routeUrl].urlPattern.test(url);
+            });
+
+            if(matchingViewUrlPattern){
+                return {
+                    matchingViewUrlPattern: matchingViewUrlPattern,
+                    viewName: urlPatterns[matchingViewUrlPattern].view,
+                    parameters: this._extractParameters(urlPatterns[matchingViewUrlPattern].urlPattern, url)
+                };
+            } else {
+                return null;
+            }
+        },
+
+        createViewByUrlPattern: function(viewByUrl){
+            var $self = this;
+            var viewByUrlPattern = {};
+            _.each(_.keys(viewByUrl), function(routeUrl){
+                var urlPattern = _.isRegExp(routeUrl)?routeUrl:$self._routeToRegExp(routeUrl);
+                viewByUrlPattern[routeUrl] = { urlPattern: urlPattern, view: viewByUrl[routeUrl] };
+            });
+            return viewByUrlPattern;
         }
     });
 });
