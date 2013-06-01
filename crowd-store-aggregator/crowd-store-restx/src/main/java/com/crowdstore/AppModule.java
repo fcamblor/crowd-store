6e53836cdc980e38396b51ff1ed9863bcc944dfd
Frédéric Camblor <fcamblor@gmail.com>
2013-06-02 00:12:26 +0200

    added restx-jongo deps + configured jongo db name in AppModule

--- crowd-store-aggregator/crowd-store-restx/src/main/java/com/crowdstore/AppModule.java
+++ crowd-store-aggregator/crowd-store-restx/src/main/java/com/crowdstore/AppModule.java
@@ -4,11 +4,19 @@ import com.google.common.base.Charsets;
 import restx.SignatureKey;
 import restx.factory.Module;
 import restx.factory.Provides;
+import restx.jongo.JongoFactory;
+
+import javax.inject.Named;
 
 @Module
 public class AppModule {
     @Provides
     public SignatureKey signatureKey() {
          return new SignatureKey("-7001706471607077523 crowd-store crowd-store 99dab1c6-4a86-4c01-9fa6-2caa7d9bb9d0".getBytes(Charsets.UTF_8));
+    }
+
+    @Provides @Named(JongoFactory.JONGO_DB_NAME)
+    public String jongoDbName(){
+        return "crowd-store";
     }
 }
